3f303dde80c5c7d676377e9627098c225e59807c
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-24 15:47:16 +0530

    ADDED: read two numbers X and Y

--- src/com/functionalprograms/Distance.java
+++ src/com/functionalprograms/Distance.java
@@ -1,9 +1,22 @@
 package com.functionalprograms;
 
+import java.util.Scanner;
+
 public class Distance {
 
 	public static void main(String[] args) {
 		System.out.println("Wecome to Distance Program");
+		
+		Scanner scanner = new Scanner(System.in);
+		// INPUT two numbers X and Y
+		int x = 0;
+		System.out.print("Enter X number : ");
+		x = scanner.nextInt();
+		int y = 0;
+		System.out.print("Enter Y number : ");
+		y = scanner.nextInt();
+		
+		System.out.println("You entered X and Y numbers are "+x+", "+y+" respectively. ");
 
 	}
 
