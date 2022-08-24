5690648721c8e89d4e597df5c9ad33024f739fac
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-24 15:30:01 +0530

    ADDED: OUTPUT triplets numbers their indexes and their values

--- src/com/functionalprograms/Triplets.java
+++ src/com/functionalprograms/Triplets.java
@@ -32,8 +32,28 @@ public class Triplets {
 		}
 		System.out.println("");
 		
+		// Store Triplets
+		int[][] triplets = new int[N*N*N][3];
+		int tripletsCounter = 0;
 		if (N < 3) {
 			System.out.println("number of distinct tripltes is 0");
+		} else {
+			for (int i = 0; i < (N-2); i++) {
+				for (int j = i+1; j < (N-1); j++) {
+					for (int k = j+1; k < N; k++) {
+						triplets[tripletsCounter][0] = i;
+						triplets[tripletsCounter][1] = j;
+						triplets[tripletsCounter][2] = k;
+						tripletsCounter++;
+					}
+				}
+			}
+		}
+		
+		// Show Triplets
+		System.out.println("number of distinct tripltes are "+tripletsCounter);
+		for ( int i = 0; i < tripletsCounter; i++) {
+			System.out.println("triplets indexes are "+triplets[i][0]+", "+triplets[i][1]+", "+triplets[i][2]+" and triplets values are "+array[triplets[i][0]]+", "+array[triplets[i][1]]+", "+array[triplets[i][2]]);
 		}
 	}
 
