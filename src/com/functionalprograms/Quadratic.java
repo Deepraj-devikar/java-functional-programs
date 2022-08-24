879d26ac954772a63b95a2b1fad573f411985bb6
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-24 16:28:15 +0530

    ADDED: Calculation of roots of x and show result
    If delta is negative then their not have real roots

--- src/com/functionalprograms/Quadratic.java
+++ src/com/functionalprograms/Quadratic.java
@@ -12,19 +12,30 @@ public class Quadratic {
 		
 		Scanner scanner = new Scanner(System.in);
 		// INPUT three numbers a, b and c
-		int a = 0;
+		double a = 0;
 		System.out.print("Enter a number : ");
-		a = scanner.nextInt();
-		int b = 0;
+		a = scanner.nextDouble();
+		double b = 0;
 		System.out.print("Enter b number : ");
-		b = scanner.nextInt();
-		int c = 0;
+		b = scanner.nextDouble();
+		double c = 0;
 		System.out.print("Enter c number : ");
-		c = scanner.nextInt();
+		c = scanner.nextDouble();
 
 		// Show Quadratic Equation
 		System.out.println("You entered a, b and c values are "+a+", "+b+" and "+c+" respectively");
 		System.out.println("And you entered quadratic equation is "+a+"*x*x + "+b+"*x + "+c);
+		
+		// Calculation
+		double delta = b * b - 4 * a * c;
+		if (delta < 0) {
+			System.out.println("Their are no real roots");
+		} else {
+			double root1X = (-b + Math.sqrt(delta)) / (2 * a);
+			double root2X = (-b - Math.sqrt(delta)) / (2 * a);	
+			System.out.println("Root 1 of X is : "+root1X);
+			System.out.println("Root 2 of X is : "+root2X);
+		}
 	}
 
 }
