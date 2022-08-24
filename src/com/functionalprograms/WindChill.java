981d6735989c72966df7eb6869ff940d2affdb63
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-24 16:37:31 +0530

    ADDED: INPUT Temprature and Wind Speed

--- src/com/functionalprograms/WindChill.java
+++ src/com/functionalprograms/WindChill.java
@@ -1,9 +1,22 @@
 package com.functionalprograms;
 
+import java.util.Scanner;
+
 public class WindChill {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Wind Chill Program");
+		
+		Scanner scanner = new Scanner(System.in);
+		// INPUT temprature t and wind speed v
+		double t = 0;
+		System.out.print("Enter temprature t in faranhite : ");
+		t = scanner.nextDouble();
+		double v = 0;
+		System.out.print("Enter wind speed v in miles per hour : ");
+		v = scanner.nextDouble();
+		
+		System.out.println("You entered temprature t is "+t+" and wind speed v is "+v);
 
 	}
 
